4964ffbc00b53196c3b59da4e6445bf075d7d04f
Daniel Morgan <me@danielmorgan.co.uk>
2017-03-21 01:23:38 +0000

    Change map style

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,9 @@ mapboxgl.accessToken = window.mapbox.key;
 
 window.map = new mapboxgl.Map({
     container: 'map',
-    center: [0, 0],
-    zoom: 14,
-    style: 'mapbox://styles/mapbox/streets-v9'
+    center: [-2.222678, 53.42683],
+    zoom: 15,
+    style: 'mapbox://styles/morgan345/cj0a1fues00b92rny9c27800q'
 });
 
 window.map.on('load', () => {
